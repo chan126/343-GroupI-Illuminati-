0e2f18844067a54fa8bccdc6c8834d58ccd99e71
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-04-30 15:31:35 -0700

    Update GroupCard.java

--- Java/src/GroupCard.java
+++ Java/src/GroupCard.java
@@ -1,15 +1,31 @@
 package cards;
 
+import java.util.ArrayList;
+
 public class GroupCard extends Card
 {
+	public int resistance;
+	public ArrayList<String> alignments;
+	
 	public GroupCard(String name, String description)
 	{
 		this.name = name;
 		this.description = description;
+		alignments = new ArrayList<String>();
 	}
 	
-	public void deposit(int amount)
+	public void setResistance(int resistance)
 	{
-		this.
+		this.resistance = resistance;
+	}
+	
+	public int getResistance()
+	{
+		return resistance;
+	}
+	
+	public void addAlignment(String alignment)
+	{
+		alignments.add(alignment);
 	}
 }
