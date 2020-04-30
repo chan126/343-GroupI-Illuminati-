f1b8015ffbb6c8f397aeec754a046c6dbd13958b
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-04-30 15:46:48 -0700

    Update UncontrolledGroups.java

--- Java/src/UncontrolledGroups.java
+++ Java/src/UncontrolledGroups.java
@@ -1,5 +1,34 @@
 package cards;
 
-public class UncontrolledGroups {
+import java.util.ArrayList;
 
+public class UncontrolledGroups
+{
+	public ArrayList<GroupCard> uncontrolled;
+	
+	public UncontrolledGroups()
+	{
+		uncontrolled = new ArrayList<GroupCard>();
+		setGroups();
+	}
+	
+	public void setGroups()
+	{
+		// add all 83 groups here
+		GroupCard AAA = new GroupCard("American Autoduel Association", "");
+		AAA.setAttackPower(1);
+		AAA.setResistance(5);
+		AAA.setIncome(1);
+		AAA.addAlignment("Violent");
+		AAA.addAlignment("Weird");
+		uncontrolled.add(AAA);
+		
+		GroupCard ANA = new GroupCard("Anti-Nuclear Activists", 
+				"+2 on any attempt to destroy Nuclear Power Companies.");
+		ANA.setAttackPower(2);
+		ANA.setResistance(5);
+		ANA.setIncome(1);
+		ANA.addAlignment("Liberal");
+		uncontrolled.add(ANA);
+	}
 }
