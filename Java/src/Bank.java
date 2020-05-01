965e52a07fe33544bf7c02a8ff47acb6e07bf329
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-04-30 22:19:02 -0700

    Update Bank.java

--- Java/src/Bank.java
+++ Java/src/Bank.java
@@ -4,33 +4,22 @@ import cards.Card;
 
 public class Bank
 {
-	public int total;
-	ArrayList<Integer> register;
+	public int bankTotal;
 	
 	public Bank()
 	{
-		total = 1106;
-		register = new ArrayList<Integer>();
-		register.add(4);  // 50s
-		register.add(20); // 20s
-		register.add(24); // 10s
-		register.add(32); // 5s
-		register.add(12); // 3s
-		register.add(28); // 2s
-		register.add(14); // 1s
+		bankTotal = 1106;
 	}
 	
-	public void withdraw(Card card, int amount)
+	public void withdraw(Card card)
 	{
-		if(amount % 50 > 1)
-		{
-			register.set(0, register.get(0) - 1);
-			card.deposit(50);
-		}
+		card.addToBalance(card.income);
+		bankTotal -= card.income;
 	}
 	
-	public void deposit()
+	public void deposit(Card card, int amount)
 	{
-		
+		card.removeFromBalance(amount);
+		bankTotal += amount;
 	}
 }
