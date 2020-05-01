5fec20204b6dae1acb75105eb651b3e9e7e11118
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-04-30 22:17:53 -0700

    Update Game.java

--- Java/src/Game.java
+++ Java/src/Game.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+
+import cards.FaceDownCards;
+import cards.IlluminatiCard;
+import cards.IlluminatiCards;
+import cards.UncontrolledGroups;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -50,7 +56,7 @@ public class Game
 			String username = in.next();
 			Player newPlayer = new Player(username);
 			players.add(newPlayer);
-			System.out.println("Added " + username + " to the game!");
+			System.out.println("Added " + username.toUpperCase() + " to the game!");
 		}
 		else if(choice == 2)
 		{
@@ -59,7 +65,7 @@ public class Game
 			int indexRemove = in.nextInt();
 			String username = players.get(indexRemove - 1).toString();
 			players.remove(indexRemove - 1);
-			System.out.println("Removed " + username + " from the game!");
+			System.out.println("Removed " + username.toUpperCase() + " from the game!");
 		}
 	}
 	
@@ -68,7 +74,8 @@ public class Game
 		System.out.println("Thanks for playing!");
 	}
 	
-	public static void sequenceOfPlay(ArrayList<Player> players)
+	// one player's turn
+	public static void sequenceOfPlay(ArrayList<Player> players, Bank bank, FaceDownCards faceDown, UncontrolledGroups uncontrolled)
 	{
 //		Play goes counter-clockwise around the table.
 //		1. Collect income on all cards that have an Income
@@ -95,6 +102,52 @@ public class Game
 //		7. Add targets. Draw cards until there are two uncontrolled Groups. Discard any Specials drawn.
 	}
 	
+	public static void playGame(ArrayList<Player> players)
+	{
+		Scanner in = new Scanner(System.in);
+		
+		//each player chooses their Illuminati card
+		IlluminatiCards illuminati = new IlluminatiCards();
+		Bank bank = new Bank();
+		FaceDownCards faceDown = new FaceDownCards(); // contains all game cards (Group and Special)
+		UncontrolledGroups uncontrolled = new UncontrolledGroups();
+		
+		for(Player player : players)
+		{
+			System.out.println("\n" + player + ", please choose your Illuminati group.");
+			int i = 0;
+			for(IlluminatiCard card : illuminati.getCards())
+			{
+				System.out.println(i+1 + ") " + card);
+				i++;
+			}
+			int choice = in.nextInt();
+			IlluminatiCard myCard = illuminati.remove(choice-1);
+			player.setIlluminati(myCard);
+			
+			System.out.println(player.toString().toUpperCase() + " has selected " 
+					+ myCard.toString().toUpperCase() + "!");
+			
+			player.collectIncome(myCard, bank);
+			System.out.println("Collecting $" + myCard.getIncome() + " from the Bank...");
+		}
+		System.out.println("\nAll players have selected their Illuminati groups.");
+		for(Player player : players)
+		{
+			System.out.println(player + ": " + player.getIlluminati());
+		}
+		
+		faceDown.shuffle(); // WRITE THIS METHOD
+		
+		// DRAW FOUR CARDS FROM FACEDOWN AND MOVE THEM TO UNCONTROLLED
+		// IF SPECIAL CARD IS DRAWN, RETURN TO FACEDOWN AND DRAW AGAIN
+		
+		for(Player player : players)
+		{
+			sequenceOfPlay(players, bank, faceDown, uncontrolled);
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -104,7 +157,7 @@ public class Game
 		while(choice == 1 || choice == 2 || choice == 3)
 		{
 			if(choice == 1)
-				sequenceOfPlay(players);
+				playGame(players);
 			else if(choice == 2)
 				settings(players);
 			else if(choice == 3)
@@ -113,8 +166,6 @@ public class Game
 				break;
 			}
 			choice = menu(players);
-		}
-		
-		System.out.println("\nPlayer List: " + Arrays.toString(players.toArray()));
+		}		
 	}
 }
