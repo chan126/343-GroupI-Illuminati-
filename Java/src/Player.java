2f7c5d22b04d6e6301b32c76e59180d2c45294b4
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-04-30 22:23:01 -0700

    Update Player.java

--- Java/src/Player.java
+++ Java/src/Player.java
@@ -1,11 +1,30 @@
+import cards.Card;
+import cards.IlluminatiCard;
 
 public class Player
 {
 	public String username;
+	public IlluminatiCard illuminati;
 	
 	public Player(String username)
 	{
 		this.username = username;
+	}
+	
+	public void setIlluminati(IlluminatiCard illuminati)
+	{
+		this.illuminati = illuminati;
+	}
+	
+	public IlluminatiCard getIlluminati()
+	{
+		return illuminati;
+	}
+	
+	public void collectIncome(Card card, Bank bank)
+	{
+		bank.withdraw(card);
+		card.addToBalance(card.getIncome());
 	}
 	
 	public String toString()
