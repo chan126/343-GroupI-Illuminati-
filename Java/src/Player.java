55fd90f5155c24ff1a4a3689ce7c950ed7a1c928
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-05-02 21:32:46 -0700

    Update Player.java

--- Java/src/Player.java
+++ Java/src/Player.java
@@ -1,14 +1,42 @@
+import java.util.ArrayList;
+
 import cards.Card;
 import cards.IlluminatiCard;
+import cards.GroupCard;
+import cards.SpecialCard;
 
 public class Player
 {
-	public String username;
-	public IlluminatiCard illuminati;
+	private String username;
+	protected IlluminatiCard illuminati;
+	protected ArrayList<GroupCard> groupCards;
+	protected ArrayList<SpecialCard> specialCards;
 	
 	public Player(String username)
 	{
 		this.username = username;
+		groupCards = new ArrayList<GroupCard>();
+		specialCards = new ArrayList<SpecialCard>();
+	}
+	
+	public void addGroupCard(GroupCard group)
+	{
+		groupCards.add(group);
+	}
+	
+	public void removeGroupCard(GroupCard group)
+	{
+		groupCards.remove(group); //???????????
+	}
+	
+	public void addSpecialCard(SpecialCard special)
+	{
+		specialCards.add(special);
+	}
+	
+	public void removeSpecialCard(SpecialCard special)
+	{
+		specialCards.remove(special); //???????????
 	}
 	
 	public void setIlluminati(IlluminatiCard illuminati)
