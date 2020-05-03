b2507d5d808ebe066475dfb2bf642ad9f66ef9e0
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-05-02 17:40:52 -0700

    Update IlluminatiCard.java

--- Java/src/IlluminatiCard.java
+++ Java/src/IlluminatiCard.java
@@ -2,20 +2,17 @@ package cards;
 import java.util.ArrayList;
 
 public class IlluminatiCard extends Card
-{
-	ArrayList<GroupCard> subjects;
-	
+{	
 	public IlluminatiCard(String name, String description)
 	{
-		this.name = name;
-		this.description = description;
-		subjects = new ArrayList<GroupCard>();
+		super(name, description);
+		out = 4;
 	}
 	
 	public void addSubject(GroupCard subject)
 	{
-		if(subjects.size() < 4)
-			subjects.add(subject);
+		if(puppets.size() < out)
+			puppets.add(subject);
 		else
 			System.out.println("ERROR: You already have 4 subjects under your Illuminati group!");
 	}
